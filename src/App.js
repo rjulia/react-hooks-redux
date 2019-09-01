bb442feefb03ba2e85deec7dc25b7d7cea54b699
Ramon Julia <reimon1972@gmail.com>
2019-09-01 12:48:28 +0800

    add handled funtiouns

--- src/App.js
+++ src/App.js
@@ -1,32 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v4';
+
 const initialTodos = [
   {
-    id: 'a',
+    id: uuid(),
     task: 'Learn React',
     complete: true,
   },
   {
-    id: 'b',
+    id: uuid(),
     task: 'Learn Firebase',
     complete: true,
   },
   {
-    id: 'c',
+    id: uuid(),
     task: 'Learn GraphQL',
     complete: false,
   },
 ];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+  const [task, setTask] = useState('');
+  const handleChangeInput = event => {
+    setTask(event.target.value);
+  };
+  const handleSubmit = event => {
+    if (task) {
+      setTodos(todos.concat({ id: uuid(), task, complete: false }));
+    }
+    setTask('');
+    event.preventDefault();
+  };
+
+  const handleChangeCheckbox = id => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, complete: !todo.complete };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
+
   return (
     <div>
       <ul>
-        {initialTodos.map(todo => (
+        {todos.map(todo => (
           <li key={todo.id}>
-            <label>{todo.task}</label>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.complete}
+                onChange={() => handleChangeCheckbox(todo.id)}
+              />
+              {todo.task}</label>
           </li>
         ))}
       </ul>
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type="text"
+          value={task}
+          onChange={handleChangeInput} />
+        <button type="submit">Add Todo</button>
+      </form>
+
     </div>
   );
 }
